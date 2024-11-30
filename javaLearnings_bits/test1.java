3e3548050f33601228d29884c72df55e10150e5f
Veena <veenamaragur@gmail.com>
2024-11-30 20:21:45 +0530

    newproject added

---
+++ javaLearnings_bits/test1.java
@@ -0,0 +1,9 @@
+package javaLearnings_bits;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Please enter the numbers");
+        byte bt=120;
+        System.out.println(bt);
+    }
+}
